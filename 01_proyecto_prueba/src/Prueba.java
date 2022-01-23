0b7d378bd8cbe965e8a6e62d15c9a4a53261c5af
Antonio Martín <ajms66@gmail.com>
2022-01-23 12:06:33 +0100

    commit 3 modificación en proyecto 1 y nuevo proyecto creado. En gitcomandos

--- 01_proyecto_prueba/src/Prueba.java
+++ 01_proyecto_prueba/src/Prueba.java
@@ -4,6 +4,8 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Primer mensaje que se escribe para versión 1");
 		System.out.println("Segundo mensaje que se escribe para versión 2 desde otrorepositorio");
+		System.out.println("Tercer mensaje que se escribe para versión 3 desde gitcomandos");
+		
 	}
 
 }
---
+++ 02_otro_proyecto_prueba/src/Prueba2.java
@@ -0,0 +1,8 @@
+
+public class Prueba2 {
+
+	public static void main(String[] args) {
+		System.out.println("mensaje 1 en nueva clase de otro proyecto");
+	}
+
+}
