55d580573d27172526f1925fd6518740865dbf49
Antonio Martín <ajms66@gmail.com>
2022-01-23 12:03:21 +0100

    commit 2 modificación desde otrorepositorio

--- 01_proyecto_prueba/src/Prueba.java
+++ 01_proyecto_prueba/src/Prueba.java
@@ -3,7 +3,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Primer mensaje que se escribe para versión 1");
-
+		System.out.println("Segundo mensaje que se escribe para versión 2 desde otrorepositorio");
 	}
 
 }
